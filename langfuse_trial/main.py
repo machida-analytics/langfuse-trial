555408df4f7a4d4767c8fe4d3b75ce5c62848b1d
Kogia-sima <orcinus4627@gmail.com>
2024-08-05 20:06:06 +0900

    Add missing packages

--- langfuse_trial/main.py
+++ langfuse_trial/main.py
@@ -11,33 +11,47 @@ langfuse_handler = CallbackHandler(
     host=os.getenv("LANGFUSE_HOST"),
 )
 
-from langchain_community.chat_models import ChatOpenAI
 from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_openai import ChatOpenAI
 
-docs = [
-    Document(page_content="Jesse loves red but not yellow"),
-    Document(page_content="Jamal loves green but not as much as he loves orange"),
-]
-vectorstore = FAISS.from_documents(docs, embedding=OpenAIEmbeddings())
-retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "以下のcontextだけに基づいて回答してください。\n{context}\n質問: {question}",
-        )
+def format_docs(docs: list[Document]) -> str:
+    return "\n".join([doc.page_content for doc in docs])
+
+
+def main():
+    docs = [
+        Document(page_content="Jesse loves red but not yellow"),
+        Document(page_content="Jamal loves green but not as much as he loves orange"),
     ]
-)
-llm = ChatOpenAI(model="gpt-3.5-turbo")
+    vectorstore = FAISS.from_documents(docs, embedding=OpenAIEmbeddings())
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
-chain = {"context": retriever, "question": RunnablePassthrough()} | prompt | llm
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "以下のcontextだけに基づいて回答してください。\n=====\n{context}\n=====\n質問: {question}",
+            )
+        ]
+    )
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0, sample=False)
 
-# Add Langfuse handler as callback (classic and LCEL)
-chain.invoke(
-    "What are everyone's favorite colors?", config={"callbacks": [langfuse_handler]}
-)
+    chain = (
+        {
+            "context": retriever | RunnableLambda(format_docs, name="FormatDocs"),
+            "question": RunnablePassthrough(),
+        }
+        | prompt
+        | llm
+    )
+
+    # Add Langfuse handler as callback (classic and LCEL)
+    result = chain.invoke(
+        "What are everyone's favorite colors?", config={"callbacks": [langfuse_handler]}
+    )
+    print(result)
